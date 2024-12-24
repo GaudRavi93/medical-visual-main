bdff85532980f6a856ea3d8b1c49360d6fc1a6ac
freelancework00700 <freelancework00700@gmail.com>
2024-12-24 10:29:08 +0530

    added ailwind css and prettier formatter

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Includes all HTML and TypeScript files in your Angular app
+  ],
+  theme: {
+    extend: {}, // You can add customizations here
+  },
+  plugins: [],
+};
